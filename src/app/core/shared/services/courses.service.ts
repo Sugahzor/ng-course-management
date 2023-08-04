df367c9113ad7907cd0ded0243d66dcb9a1cf44c
IuliaV <iuliamvoicu@yahoo.com>
2023-08-04 16:29:32 +0300

    add JWT to courses calls

--- src/app/core/shared/services/courses.service.ts
+++ src/app/core/shared/services/courses.service.ts
@@ -19,15 +19,27 @@ export class CoursesService {
   constructor(private http: HttpClient) {}
 
   getCourses(): Observable<CourseDTO[]> {
-    return this.http.get<CourseDTO[]>(this.FULL_COURSES_URL);
+    return this.http.get<CourseDTO[]>(this.FULL_COURSES_URL, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+      },
+    });
   }
 
   getCourseDetails(courseId: number) {
-    return this.http.get<CourseDTO>(`${this.FULL_COURSES_URL}/${courseId}`);
+    return this.http.get<CourseDTO>(`${this.FULL_COURSES_URL}/${courseId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+      },
+    });
   }
 
   addLessonsToCourse(curriculum: CurriculumCreationDTO): Observable<CourseDTO> {
-    return this.http.put<CourseDTO>(this.FULL_COURSES_URL, curriculum);
+    return this.http.put<CourseDTO>(this.FULL_COURSES_URL, curriculum, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+      },
+    });
   }
 
   removeLessonFromCourse(
@@ -35,7 +47,12 @@ export class CoursesService {
     lessonId: number
   ): Observable<CourseDTO> {
     return this.http.delete<CourseDTO>(
-      `${this.FULL_COURSES_URL}/${courseId}/lessonId/${lessonId}`
+      `${this.FULL_COURSES_URL}/${courseId}/lessonId/${lessonId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+        },
+      }
     );
   }
 
@@ -48,6 +65,10 @@ export class CoursesService {
   }
 
   deleteCourse(courseId: number) {
-    return this.http.delete(`${this.FULL_COURSES_URL}/${courseId}`);
+    return this.http.delete(`${this.FULL_COURSES_URL}/${courseId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+      },
+    });
   }
 }
